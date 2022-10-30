b00069fc9daf260262d0ede6acc26e00dca8318b
simeydotme <simey.me@gmail.com>
2022-10-30 14:18:00 +0800

    replace GA codes with env variables

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    watch: {
-      usePolling: true
+export default defineConfig(({mode}) => {
+
+  const venv = loadEnv(mode, process.cwd(), '')
+  const env = Object.keys(venv).filter((item) => item.startsWith("VITE_")).reduce((cur, key) => { return Object.assign(cur, { [key]: venv[key] })}, {}) ;
+
+  const htmlPlugin = () => {
+    return {
+      name: "html-transform",
+      transformIndexHtml(html) {
+        return html.replace(/%(.*?)%/g, function (match, p1) {
+          return env[p1];
+        });
+      },
+    };
+  };
+
+  return {
+    plugins: [svelte(), htmlPlugin()],
+    server: {
+      watch: {
+        usePolling: true
+      }
     }
   }
-})
+});
