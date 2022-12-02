f22bdf71507eee6c01d82d0110a94a46e44d1914
dub <santiagodgalvan@gmail.com>
2022-12-02 00:38:03 -0300

    adding new contract

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,8 @@
 	import { Account, Contract, ec, number, uint256 } from "starknet";
 	import { connect, disconnect } from "get-starknet"
 
-	const POKEMON_CONTRACT_ADDRESS = "0x028345627f0e0301417fc654b629a3e241675903784cf3e7ec8e1042c8e476be"
+	const POKEMON_CONTRACT_ADDRESS = "0x02a1e5176c4d391fde798f5739fed081901710377eecffe493a46c8ad880fc39"
+	// const OG_POKEMON_CONTRACT_ADDRESS = "0x028345627f0e0301417fc654b629a3e241675903784cf3e7ec8e1042c8e476be"
 	const ipfs_url = "https://ipfs.io/ipfs/QmbCRMSuCDxxXGRNgvAM3BhDVNC6i8hvCT2NvpnsqgFQhS/"
     const CARDS_DECK = 69;
 
@@ -50,7 +51,7 @@
 	const mintDailyCards = async() => {
 		try {
 			pokemonContract = new Contract(contract.abi, POKEMON_CONTRACT_ADDRESS, provider);
-			await pokemonContract.mintDailyCards()
+			await pokemonContract.mint_daily_cards()
 		} catch (error) {
 			console.log(error)
 		}
@@ -80,7 +81,7 @@
 	};
 
 	const getCardsMintedToday = async () => {
-		let cardsMinted = await pokemonContract.user_claim_pack(address)
+		let cardsMinted = await pokemonContract.get_user_claimed_pack(address)
 		let cards = []
 
 		let cardsInString = cardsMinted.toString()
@@ -173,12 +174,18 @@
 				</p>
 			</section>
 
-			<p class="author">This frontend is an adaptation made by me of the work of <br /><a href="https://twitter.com/simeydotme"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg> @simeydotme</a> |
+			<p class="author">This frontend is an adaptation of <br /><a href="https://twitter.com/simeydotme"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg> @simeydotme</a> |
 				<em><a href="https://github.com/simeydotme/pokemon-cards-css"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg> Simon Goellner</em>
 				<p class="author">Backend in Cairo by <a href="https://twitter.com/dub_zn"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg>@dub_zn</a>
 			</p>
 			{#if !walletModalVisible}
 				<div class="showcase">
+					<Card 
+						img={"https://i.ibb.co/yqKsRHQ/gengar-cairo.png"}
+						rarity="Rare Ultra" />
+				</div> 
+			{:else}
+				<div class="showcase" style="z-index: 0">
 					<Card 
 						img={"https://i.ibb.co/yqKsRHQ/gengar-cairo.png"}
 						rarity="Rare Ultra" />
@@ -231,79 +238,6 @@
 					{/each}
 				{/if}
 			</CardListDaily>
-			<!-- <div class="header-inside-minted">
-				{#if isConnected && !isLoadingMintedToday && mintedTodayCards.length == 0}
-					<div class="minted-today"
-					on:click={() => mintDailyCards()}
-					>
-						<div class="minted-text-has-stock">READY TO CLAIM</div>
-						<CardPackGlowing 
-							img={"https://crystal-cdn2.crystalcommerce.com/photos/352236/base_set.jpg"}
-							rarity="Rare Holo V"
-
-						/>
-					</div>
-					{#each Array(5) as _, i}
-						<div class="minted-today">
-							<CardMinted 
-							/>
-						</div>
-					{/each}
-				{:else if !isConnected || isLoadingMintedToday}
-					<div class="minted-today">
-						<div>
-							<CardPackOff 
-								img={"https://crystal-cdn2.crystalcommerce.com/photos/352236/base_set.jpg"}
-								rarity="Common"
-							/>
-						</div>
-					</div>
-					{#each Array(5) as _, i}
-						<div class="minted-today">
-							<CardMinted 
-							/>
-						</div>
-					{/each}
-				{:else}
-					<div class="minted-today">
-						<div>
-							<div class="minted-text-out-stock">OUT OF STOCK</div>
-							<CardPackOff 
-								img={"https://crystal-cdn2.crystalcommerce.com/photos/352236/base_set.jpg"}
-								rarity="Common"
-							/>
-						</div>
-					</div>
-				{/if}
-				{#if !isLoadingMintedToday && mintedTodayCards.length > 0}
-					{#each mintedTodayCards as card_id, i}
-						{#if card_id <= 15}
-						<div class="minted-today">
-							<CardMinted 
-								img={ipfs_url+"/"+ card_id +".webp"}
-								rarity="Rare Holo V"
-							/>
-						</div>
-						{/if}
-						{#if card_id > 15}
-						<div class="minted-today">
-							<CardMinted 
-								img={ipfs_url+"/"+ card_id +".webp"}
-								rarity="Common"
-							/>
-						</div>
-						{/if}
-					{/each}
-					{#if mintedTodayCards.length == 0}
-						{#each Array(5) as _, i}
-							<div class="minted-today">
-								<CardMinted 
-								/>
-							</div>
-						{/each}
-					{/if}
-				{/if}
-			</div> -->
 		</div>
 	</header>
 
@@ -360,6 +294,7 @@
 							quantity={card.quantity}
 							rarity="Common"
 						/>
+						
 					{/if}
 				{/if}
 			{/each}
