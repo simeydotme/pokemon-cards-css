d23df46205ed6a5f0df74686e954b25947ea4f08
dub <santiagodgalvan@gmail.com>
2022-12-12 00:10:38 -0300

    edit metadata

--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,11 @@
 	import { stringToFeltArray } from "./utils/utils.js";
 	
 	import { Account, Contract, ec, number, uint256, defaultProvider } from "starknet";
-	import { connect, disconnect, getInstalledWallets, getStarknet } from "get-starknet"
+	import { connect, disconnect } from "get-starknet"
+	
+	const POKEMON_CONTRACT_ADDRESS = "0x0344eaf4ccc9f9a4fcccf9140a1cd0820e5a06bccd697847f863fbe733b672f0"
 
-	const POKEMON_CONTRACT_ADDRESS = "0x07927d282802f2dc3904d4e010a0d7ce983646837da74ed89dba78732ceb663f"
+	// const POKEMON_CONTRACT_ADDRESS = "0x07927d282802f2dc3904d4e010a0d7ce983646837da74ed89dba78732ceb663f"
 	// const POKEMON_CONTRACT_ADDRESS = "0x02a1e5176c4d391fde798f5739fed081901710377eecffe493a46c8ad880fc39"
 	const ipfs_url = "https://ipfs.io/ipfs/QmbCRMSuCDxxXGRNgvAM3BhDVNC6i8hvCT2NvpnsqgFQhS/"
     const CARDS_DECK = 69;
@@ -44,13 +46,9 @@
 	const connectWallet = async() => {   
 		try{
 			walletModalVisible = true
-    		// await windowStarknet.enable()
 			const starknet = await connect( {modalOptions: {theme: "dark"}} );     
 			await starknet?.enable({  starknetVersion: "v4" })     
 
-			console.log("AFRTER")
-			console.log(await getInstalledWallets())
-			console.log(await getStarknet())
 			provider = starknet.account     
 			address = starknet.selectedAddress    
 			isConnected = true    
@@ -272,21 +270,21 @@
 
 	init()
 
-	// console.log(stringToFeltArray("https://gateway.pinata.cloud/ipfs/QmdGwt12YLuJQkbENy3VR1hoRZKCfAXZAbPwXZNZMSXQ7L/"))
+	// console.log(stringToFeltArray("https://gateway.pinata.cloud/ipfs/QmfFAsU3zv6QYLQY4SNEfdjznhS2T2mW1gQDDG4bNvvuBy/"))
 
-	// onMount(() => {
-	// 	const $headings = document.querySelectorAll("h1,h2,h3");
-	// 	const $anchor = [...$headings].filter((el) => {
-	// 		const id = el.getAttribute("id")?.replace(/^.*?-/g,"");
-	// 		const hash = window.location.hash?.replace(/^.*?-/g,"")
-	// 		return id === hash;
-	// 	})[0];
-	// 	if( $anchor ) {
-	// 		setTimeout(() => {
-	// 			$anchor.scrollIntoView();
-	// 		},100);
-	// 	}
-	// });
+	onMount(() => {
+		const $headings = document.querySelectorAll("h1,h2,h3");
+		const $anchor = [...$headings].filter((el) => {
+			const id = el.getAttribute("id")?.replace(/^.*?-/g,"");
+			const hash = window.location.hash?.replace(/^.*?-/g,"")
+			return id === hash;
+		})[0];
+		if( $anchor ) {
+			setTimeout(() => {
+				$anchor.scrollIntoView();
+			},100);
+		}
+	});
 
 </script>
 
@@ -312,8 +310,9 @@
 				</p>
 			</section>
 
-			<p class="author">This frontend is an adaptation of <br /><a href="https://twitter.com/simeydotme" style="color: rgb(66, 219, 240);"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg> @simeydotme</a> |
-				<em><a href="https://github.com/simeydotme/pokemon-cards-css" style="color: rgb(66, 219, 240);" ><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg> Simon Goellner</em>
+			<p class="author">
+				This frontend is an adaptation of <a href="https://twitter.com/simeydotme" style="color: rgb(66, 219, 240);"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg> @simeydotme</a> |
+				<em><a href="https://github.com/simeydotme/pokemon-cards-css" style="color: rgb(66, 219, 240);" ><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg> Simon Goellner</em> work.
 				<p class="author">Backend in Cairo by <a href="https://twitter.com/dub_zn" style="color: rgb(66, 219, 240);"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/> </svg>@dub_zn</a>
 			</p>
 			{#if !walletModalVisible}
