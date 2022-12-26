a7d46a758bddbfdb2854f1264ad66e2493a47625
simeydotme <simey.me@gmail.com>
2022-12-27 00:53:41 +0800

    add secret rare standalone css

--- src/lib/components/card-glare.svelte
+++ src/lib/components/card-glare.svelte
@@ -13,18 +13,6 @@
 
 
 
-	:global(.card[data-rarity="rare secret"]) .card__glare {
-		background-image: 
-			radial-gradient( 
-				circle at var(--pointer-x) var(--pointer-y), 
-				hsla(40, 100%, 97%, 0.95) 0%, 
-				hsla(32, 100%, 87%, 0.45) 30%, 
-				hsla(0, 0%, 0%, 0.75) 80% 
-			);
-			opacity: calc( var(--card-opacity) * 0.6 );
-			filter: brightness(.9) contrast(1.8);
-	}
-
 :global(.card[data-rarity*="rare holo v"][data-gallery="true"]) .card__glare {
 	background-image: 
 		radial-gradient( 
